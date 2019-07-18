19b9cf4c895cb23dd75f5d72102cc337c00a1fe0
Anirach Mingkhwan <anirachmcpro@Anirachs-MacBook-Pro.local>
2019-07-18 16:37:08 +0700

    Define Brain and BrainLink

---
+++ brain.py
@@ -0,0 +1,8 @@
+MyBrain = {'man': 3, 'woman': 4, 'year': 1, 'baby': 2}
+BrainLink = {'man-woman': 3, 'man-baby': 1, 'man-year': 1}
+print(MyBrain.items())
+print(MyBrain.values())
+print(MyBrain.keys())
+if 'man-baby' in BrainLink:
+    print(MyBrain.__sizeof__())
+print(max(MyBrain.values()))
