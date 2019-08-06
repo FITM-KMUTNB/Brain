49c41e9058bc6e01ff17819f26a25e6115bec625
Anirach Mingkhwan <anirachmcpro@Anirachs-MacBook-Pro.local>
2019-08-06 10:03:20 +0700

    done word and word_pair frequency count...

--- OpenText.py
+++ OpenText.py
@@ -6,7 +6,7 @@ import glob
 import os
 
 MyBrain = dict()
-BrianLink = dict()
+BrainLink = dict()
 
 # Main function process file in directoty
 
@@ -15,7 +15,7 @@ def main():
     listfile("/Users/anirachmcpro/Desktop/Brain")
     print("="*20, 'Summaries', "="*20)
     print(MyBrain)
-    print(BrianLink)
+    print(BrainLink)
 
 
 # List all the text file in the directory
@@ -34,47 +34,57 @@ def listline(fname):
     c_file = open(fname, 'r')
     # Read all the lines from the file.
     for line in c_file:
-        print(line)
         processline(line)
-        print(MyBrain)
-    # Close the file.
     c_file.close()
 
+
 # Processing line
-
-
 def processline(w_line):
     global MyBrain
-    global BrianLink
+    global BrainLink
     wordlists = w_line.split()
-    print(wordlists)
+    # count word frequencies
     for word in wordlists:
         count = wordlists.count(word)
-
-        # Count Word frequencies
         if word in MyBrain.keys():
             MyBrain[word] += count
         else:
             MyBrain[word] = count
-        # Count word_pairs frequencies
-        # here have to be functuion swap compare
-        # and pair_word order by less-more
-        if link_exist(word_pair):
-            BrainLink[word_pair] += count
-        else:
-            Brain_Link[word_pair] = count
+    # Count word_pairs frequencies
+    for i in range(len(wordlists)):
+        if i+1 == len(wordlists):
+            break
+        for j in range(i, len(wordlists)):
+            if j+1 == len(wordlists):
+                break
+            word_pair = wordlists[i]+'-' + wordlists[j+1]
+            if link_exist(word_pair):
+                BrainLink[word_pair] += count
+            else:
+                BrainLink[word_pair] = count
 
 
 def link_exist(wordlink):
-    global BrianLink
+    global BrainLink
     wlist = wordlink.split('-')
     wlrev = wlist[1]+wlist[0]
-    if wordlink in BrianLink.keys():
+    if wordlink in BrainLink.keys():
         return True
     elif wlrev in BrainLink.keys():
         return True
     else:
         return False
+
+
+# Neo4j - Create graph database
+def create_node():
+    global MyBrain
+    pass
+
+
+def create_link():
+    global BrainLink
+    pass
 
 
 # Call the main function.
